b1b65935be5750a4ac426b01be09671177c36568
way_wu <way_wu@iplaystar.net>
2025-04-08 11:44:03 +0800

    重構 play.py，新增時間戳記功能並隨機生成字母，改善輸出格式

--- play.py
+++ play.py
@@ -1,5 +1,12 @@
 import os
 from play_mp3 import play_mp3
+from datetime import datetime
+import random
+import string
+
+def get_current_time():
+    return datetime.now().strftime("[%Y-%m-%d %H:%M:%S]")
+
 file = open('example.txt', 'r', encoding='utf-8')
 line = file.readline()
 
@@ -10,7 +17,14 @@ save_len=int(line_save.strip())
 total_lines = sum(1 for _ in open('example.txt', 'r', encoding='utf-8'))
 for i in range(total_lines):
     if i > save_len:
-        print(line)
+        #line去掉/n
+        line = line.strip()
+        
+        for j in range(0, len(line), 10):
+            random_words = ''.join(random.choices(string.ascii_letters, k=5))
+            random_words1 = ''.join(random.choices(string.ascii_letters, k=5))
+            print(get_current_time()+" "+random_words+"/"+line[j:j+10]+"/"+random_words1)
+        
         file_mp3=f"./save/{(i+1)}.mp3"
         play_mp3(file_mp3)
         with open('play_save.txt', 'w') as f:
